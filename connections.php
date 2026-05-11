4b2559b4daf4cb3b6597e44b9d1529e8cc69f9cc
prorenzi1213-maker <prorenzi1213@gmail.com>
2026-05-11 09:15:01 +0800

    Update connections.php

--- connections.php
+++ connections.php
@@ -3,32 +3,25 @@
 if (getenv('MYSQLHOST')) {
 
     // RAILWAY
-    $host = $_ENV['MYSQLHOST'];
-    $dbname = $_ENV['MYSQLDATABASE'];
-    $username = $_ENV['MYSQLUSER'];
-    $password = $_ENV['MYSQLPASSWORD'];
-    $port = $_ENV['MYSQLPORT'];
+    $host = getenv('MYSQLHOST');
+    $user = getenv('MYSQLUSER');
+    $pass = getenv('MYSQLPASSWORD');
+    $db   = getenv('MYSQLDATABASE');
+    $port = getenv('MYSQLPORT');
 
 } else {
 
     // LOCAL (XAMPP)
     $host = 'localhost';
-    $dbname = 'teachfinder_db';
-    $username = 'root';
-    $password = '';
+    $user = 'root';
+    $pass = '';
+    $db   = 'teachfinder_db';
     $port = 3306;
 }
 
-try {
-    $pdo = new PDO(
-        "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8",
-        $username,
-        $password
-    );
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-} catch (PDOException $e) {
-    die("DB Connection failed: " . $e->getMessage());
+if ($conn->connect_error) {
+    die("DB Connection failed: " . $conn->connect_error);
 }
 ?>
